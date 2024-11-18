4f0ca244c4c99b801c75e27b9f7618ed30d48110
Alain Bonneau <alaindev97@gmail.com>
2024-11-18 08:01:48 +0100

    add backend api

--- src/lib/axios/axios.ts
+++ src/lib/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9000/api/v1', // Changer une fois le déploiement en production
+  baseURL: 'https://fureverhome-backend-c50cba18c4a6.herokuapp.com/api/v1', // Changer une fois le déploiement en production
   headers: {
     'Content-Type': 'application/json',
   },
